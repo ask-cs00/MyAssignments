a9099b9e59cc092f4170393d00ca5da8cf49643c
ask-cs00 <anjana_sk15@yahoo.in>
2025-05-26 21:29:19 +0530

    day01_primitive dataTypes_assignment

---
+++ dataTypes_May24.js
@@ -0,0 +1,14 @@
+let firstName,companyName;
+const mobileNumber = 9876543210;
+firstName = "Atchaya";
+companyName = "Yahoo"
+let isAutomation = true ;
+let hasPlaywright;
+let accountNumber =null;
+
+console.log(" data type of  firstName: " + firstName + "-" + typeof(firstName));
+console.log(" data type of  companyName: " + companyName + "-" + typeof(companyName));
+console.log(" data type of  mobileNumber: " + mobileNumber + "-" + typeof(mobileNumber));
+console.log(" data type of  mobileNumber: " + isAutomation + "-" + typeof(isAutomation));
+console.log(" data type of  hasPlaywright: " + hasPlaywright + "-" + typeof(hasPlaywright));
+console.log(" data type of  hasPlaywright: " + accountNumber + "-" + typeof(accountNumber));
